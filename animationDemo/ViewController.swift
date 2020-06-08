ea41b838c86295a547300298c09d0f6a8a43c6af
Anmol Singh <anmolsingh1597@gmail.com>
2020-06-08 18:44:17 -0400

    [Anmol Singh] Commit for resources

--- animationDemo/ViewController.swift
+++ animationDemo/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
